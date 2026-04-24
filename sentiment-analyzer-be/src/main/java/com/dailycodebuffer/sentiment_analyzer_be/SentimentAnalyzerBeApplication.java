2bd0d23a0a60ddc0557428abe063d7801c3fba70
UMANG AGRAWAL <umangagrawal@UMANGs-MacBook-Air.local>
2026-04-24 20:33:38 +0530

    Setup backend

---
+++ sentiment-analyzer-be/src/main/java/com/dailycodebuffer/sentiment_analyzer_be/SentimentAnalyzerBeApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.sentiment_analyzer_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAnalyzerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentAnalyzerBeApplication.class, args);
+	}
+
+}
---
+++ sentiment-analyzer-be/src/test/java/com/dailycodebuffer/sentiment_analyzer_be/SentimentAnalyzerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.sentiment_analyzer_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentAnalyzerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
